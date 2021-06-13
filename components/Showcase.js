5e67e4124fc9a8b9145bea373cf2cb574b568362
dutt23 <shatyaki_dutt@hotmail.com>
2021-06-13 18:29:31 +0530

    Add component

---
+++ components/Showcase.js
@@ -0,0 +1,9 @@
+import styles from '@styles/Showcase.module.css'
+export default function Showcase() {
+  return (
+    <div className={styles.showcase}>
+      <h1>Welcome to the page</h1>
+      <h2>Find your event</h2>
+    </div>
+  )
+}
