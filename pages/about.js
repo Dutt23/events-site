dc975db30fbfbed8bff230873295df03217649f4
bima-shatyakidutt <shatyaki.dutt@milvik.se>
2021-06-13 16:49:42 +0530

    init project

---
+++ pages/about.js
@@ -0,0 +1,10 @@
+export default function AboutPage() {
+	return (
+		<div>
+			<h1>About</h1>
+			<p>This is a app to find the latest aj and other musical events</p>
+			<p>Version: 1.0.0</p>
+		</div>
+	)
+}
+
---
+++ pages/events/[slug].js
@@ -0,0 +1,7 @@
+export default function EventPage() {
+  return (
+    <div>
+      <h1>This is a Event Page</h1>
+    </div>
+  )
+}
