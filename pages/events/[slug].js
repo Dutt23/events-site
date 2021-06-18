cb387a87c11829c451cc6b67a060e0b4686f8045
dutt23 <shatyaki_dutt@hotmail.com>
2021-06-18 23:03:17 +0530

    integrate cms backend

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -26,13 +26,13 @@ export default function EventPage({ evt }) {
           </a>
         </div>
         <span>
-          {evt.date} at {evt.time}
+          {new Date(evt.date).toLocaleDateString('en-US')} at {evt.time}
         </span>
         <h1>{evt.name}</h1>
         {evt.image && (
           <div className={styles.image}>
             <Image 
-            src={evt.image}
+            src={evt.image ? evt.image.formats.medium.url : '/images/event-default.png'}
             width={960}
             height={600}
             />
@@ -56,7 +56,7 @@ export default function EventPage({ evt }) {
 
 
 export async function getStaticPaths(){
-  const res = await fetch(`${API_URL}/api/events`);
+  const res = await fetch(`${API_URL}/events`);
   const events = await res.json();
   const paths = events.map((evt) => ({
     params: {slug: evt.slug}
@@ -74,7 +74,7 @@ export async function getStaticPaths(){
 // Hence the name here has to be the same
 export async function getStaticProps({ params: 
   { slug } }) {
-  const res = await fetch(`${API_URL}/api/events/${slug}`)
+  const res = await fetch(`${API_URL}/events?slug=${slug}`)
 
   const events = await res.json()
   return {
