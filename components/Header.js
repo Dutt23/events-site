2a9e85ed0d61c517a7b6222b6d10ecad999fa105
dutt23 <shatyaki_dutt@hotmail.com>
2021-06-20 01:16:44 +0530

    Add auth context

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,19 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import Link from 'next/link'
 import styles from '@styles/Header.module.css'
 import Search from './Search'
+import AuthContext from '@context/AuthContext'
 import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa'
+import { toast } from 'react-toastify'
 export default function Header() {
+  const { user, logout } = useContext(AuthContext)
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
-      <Link href="/">
-        {/* Styling for the link gets put on the a tag */}
-        <a> DJ Events</a>
-      </Link> 
+        <Link href="/">
+          {/* Styling for the link gets put on the a tag */}
+          <a> DJ Events</a>
+        </Link>
       </div>
 
       <Search />
@@ -22,23 +25,33 @@ export default function Header() {
               <a>Events</a>
             </Link>
           </li>
-          <li>
-            <Link href="/events/add">
-              <a>Add Event</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/account/login">
-              <a className="btn-secondary btn-icon">
-                <FaSignInAlt /> Login
+
+          {user ? (
+            // If logged in
+            <> <li>
+              <Link href="/events/add">
+                <a>Add Event</a>
+              </Link>
+            </li>
+              <li>
+                <Link href="/events/dashboard">
+                  <a>Dashboard</a>
+                </Link>
+              </li>
+              <li>
+                <button onClick={() => logout()} className="btn-secondary btn-icon">
+                  <FaSignOutAlt /> Logout
+                </button>
+              </li>
+            </>) : (
+              // If logged out
+              <><li>
+                <Link href="/account/login">
+                  <a className="btn-secondary btn-icon">
+                    <FaSignInAlt /> Login
               </a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/account/register">
-              <a>Add Event</a>
-            </Link>
-          </li>
+                </Link>
+              </li></>)}
         </ul>
       </nav>
     </header>
