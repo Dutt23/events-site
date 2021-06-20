cbac70e0a84d34f679171382099e829953aec20a
dutt23 <shatyaki_dutt@hotmail.com>
2021-06-20 18:08:14 +0530

    logout functionality

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -29,7 +29,6 @@ export const AuthProvider = ({ children }) => {
     })
 
     const data = await res.json()
-    console.log(data)
     if (res.ok) {
       setUser(data.user)
       router.push('/accounts/dashboard')
@@ -40,16 +39,23 @@ export const AuthProvider = ({ children }) => {
   }
   // Logout user
   const logout = async () => {
-    console.log("Logout")
+    const res = await fetch(`${NEXT_URL}/api/logout`, {
+      method: 'POST'
+    })
+
+    if (res.ok) {
+      setUser(null)
+      router.push('/')
+    }
   }
   // Check if user is logged in
   const checkUserLoggedIn = async () => {
     const res = await fetch(`${NEXT_URL}/api/user`)
     const data = await res.json();
 
-    if(res.ok){
+    if (res.ok) {
       setUser(data.user)
-    }else {
+    } else {
       setUser(null)
       setError(data.message)
       setError(null)
